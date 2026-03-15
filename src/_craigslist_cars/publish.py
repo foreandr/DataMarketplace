0d8e0a60999c18dd299ee27d68e97f0ff821024e
Andre <foreandr@gmail.com>
2026-03-15 15:57:55 -0400

    PUSH THE V2 THINGH

--- src/_craigslist_cars/publish.py
+++ src/_craigslist_cars/publish.py
@@ -1,4 +1,27 @@
-"""Publish logic for _craigslist_cars."""
-from __future__ import annotations
+import os
+from dotenv import load_dotenv
+from hyperSel import instance, log
 
-# TODO: implement publishing to RapidAPI, Zyla, etc.
+load_dotenv()
+
+RAPID_API_EMAIL = os.getenv("RAPID_API_EMAIL")
+RAPID_API_PASSW = os.getenv("RAPID_API_PASSW")
+
+def sign_in_process(browser):
+    # CLICK SIGN IN
+    pass
+
+def main():
+    print(RAPID_API_EMAIL)
+    print(RAPID_API_PASSW)
+
+    browser = instance.Browser(
+        headless=False,
+        zoom_level=100
+    )
+    browser.init_browser()
+    browser.go_to_site("https://rapidapi.com/")
+    input("stop")
+
+if __name__ == "__main__":
+    main()
